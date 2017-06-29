0ff66f0f089bd90c8221357e292b17dfbd12c1dd
System <none>
2017-06-29 18:59:46 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var express = require('express');
+var http = require('http');
 var app = express();
 
 app.use(express.static('public'));
@@ -8,7 +9,13 @@ var ans = {};
 
 app.get('*', (req, res) => {
   
-  console.log(req.params)
+  //console.log(req.params)
+  
+  var options = {method: 'HEAD', host: req.params}
+  
+  var reqq = http.request(options, function(response) {
+    console.log(JSON.stringify(response));
+  });
 });
 
 app.listen(process.env.PORT, function () {
